ef49e6a80599c81a4798dc14706cdacebffb50e9
kuldeepkumar <mohankuldeep1234@gmail.com>
2024-06-05 21:34:14 +0530

    mongoDBURI change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
   path:'./env'
 })
-connectDB();
+connectDB()
+.then(()=>
+{
+  app.listen(process.env.PORT||8000,()=>
+  {
+    console.log(`app is listening on port ${process.env.PORT}`);
+  })
+})
+.catch((err)=>
+{
+  console.log("mongo err",err);
+});
 
 /*
 const app = express();
