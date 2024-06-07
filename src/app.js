1b7ee609beb109e8e93cd64ba91e3ac91fd6aa02
kuldeepkumar <mohankuldeep1234@gmail.com>
2024-06-07 12:22:33 +0530

    register route created

--- src/app.js
+++ src/app.js
@@ -19,4 +19,13 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users",userRouter)
+
 export { app }
